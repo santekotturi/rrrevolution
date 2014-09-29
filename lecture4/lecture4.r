5d72919ba557168747d216dfad421b8869b52447
skotturi <skotturi@ucsd.edu>
2014-09-29 00:29:18 -0700

    added lecture-4 material

--- lecture4/lecture4.r
+++ lecture4/lecture4.r
@@ -53,25 +53,77 @@ importData = function(listOfFilesToImport){
 }
 
 # and now let's actually import the data by running the script: 
-yoda = importData(myFiles)
+myData = importData(myFiles)
 
 # let's pause for a second, lines 6-29 do the same thing as lines 38-56
 # so why take the time to learn how to use importData() ? 
 # what happens if we add another file? 
 # what happens when we change the file names?
 
-years = split(yoda, yoda$year)
+years = split(myData, myData$year)
+# years is a list! we just learned all about lists!!
 
-# let's keep our analysis simple, let's just find the average air temp and mer.winds for each year
+# let's keep our analysis simple, let's just find the average and variance air temp  for each year
+# but before we jump into a for loop, we should write our code to analyze the first year
+# then worry about expanding our code to analyze all the years:
 
-for(i in seq_along())
+firstYear = years[[1]]
+head(firstYear)
+# one thing, when we imported the csv files, the air temp values
+# got converted into strings (words), they weren't kept as numbers. 
+# this is annoying but we can change it:
+firstYear$air.temp. = as.numeric(firstYear$air.temp.)
+meanAirTemp = mean(firstYear$air.temp.)
+varAirTemp = var(firstYear$air.temp.)
+
+# ok, now let's copy our code and put it inside a for loop
+# then we can work on converting it, making it work inside our for loop
+
+# and we'll need a list to store our values in:
+results = list()
+for(i in seq_along(years)){
+  x = years[[i]]
+  x$air.temp. = as.numeric(x$air.temp.)
+  meanAirTemp = mean(x$air.temp., na.rm = T)
+  varAirTemp = var(x$air.temp., na.rm = T)
+  
+  year = x$year[1]
+  data = as.data.frame(cbind(year, meanAirTemp, varAirTemp))
+  
+  results[[i]] = data
+}
 
 
+# the last step is turn our results list into an easy to read dataframe:
+# before:
+head(results)
+# after:
+resultsDF = do.call(rbind, results)
+head(resultsDF)
 
+# that wraps up Split.Analyze.Together
 
+# there are other ways of doing this including:
+unsplit(lapply(split()))
+# and 
+ddply()
 
+# in ddply it would be: 
 
+meanAndVar = function(x){
+  x$air.temp. = as.numeric(x$air.temp.)
+  meanAirTemp = mean(x$air.temp., na.rm = T)
+  varAirTemp = var(x$air.temp., na.rm = T)
+  year = x$year[1]
+  data = as.data.frame(cbind(year, meanAirTemp, varAirTemp))
+  return(data)
+}
 
+require(plyr)
+resultsDF = ddply(myData, .(year), meanAndVar)
+
+# ddply is great but I really recommend taking the time to learn 
+# how to write each part before using a powerful tool like ddply
 
 
 
