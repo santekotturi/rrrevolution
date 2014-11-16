3b58ec5fdd6d6e87b18c4ea0ef3720b89569d596
skotturi <skotturi@ucsd.edu>
2014-11-15 21:14:55 -0800

    added first part of rshiny

---
+++ lecture10/lecture10.r
@@ -0,0 +1,17 @@
+# today we'll be covering some fun R topics including:
+# 1. r on the web, rShiny, a way to code and collaborate 
+# 2. getting data off the web to analyze the world using twitter 
+
+
+# let's get started with rShiny
+# all this is coming from: http://shiny.rstudio.com/tutorial/
+
+install.packages("shiny")
+library(shiny)
+runExample("01_hello")
+
+
+# make sure you've set your working directory so the "ourApp" folder is visible 
+# then you can run:
+runApp("ourApp")
+runApp("ourApp", display.mode="showcase")
